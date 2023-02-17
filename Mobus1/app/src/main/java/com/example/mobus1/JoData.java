1ff0b7960409f4eb465591c9b91702cf84067970
Badal Samal <122680567+BadalSamal08@users.noreply.github.com>
2023-02-17 05:59:11 +0530

    2nd commit

--- Mobus1/app/src/main/java/com/example/mobus1/JoData.java
+++ Mobus1/app/src/main/java/com/example/mobus1/JoData.java
@@ -5,22 +5,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class JoData extends AppCompatActivity {
 
     private Button startB;
+    EditText editText1,editText2;
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jo_data);
+
+        editText1=findViewById(R.id.fromeF);
+        editText2=findViewById(R.id.ToF);
+
+
+
 
         startB = findViewById(R.id.start);
         startB.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
+                String value1 = editText1.getText().toString().trim();
+                String value2 = editText2.getText().toString().trim();
                 startActivity(new Intent(JoData.this, journey.class));
 
             }
--- Mobus1/app/src/main/java/com/example/mobus1/journey.java
+++ Mobus1/app/src/main/java/com/example/mobus1/journey.java
@@ -1,15 +1,29 @@
 package com.example.mobus1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class journey extends AppCompatActivity {
+    private Button startC;
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
         setContentView(R.layout.journey_start);
+
+        startC = findViewById(R.id.button2);
+        startC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(journey.this, pay.class));
+
+            }
+        });
     }
+
 }
