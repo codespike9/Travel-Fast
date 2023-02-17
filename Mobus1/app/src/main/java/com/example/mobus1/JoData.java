9a663351de913ecf4f983fa00cb38984307e1096
Badal Samal <122680567+BadalSamal08@users.noreply.github.com>
2023-02-17 10:30:01 +0530

    3rd commit

--- Mobus1/app/src/main/java/com/example/mobus1/JoData.java
+++ Mobus1/app/src/main/java/com/example/mobus1/JoData.java
@@ -12,7 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class JoData extends AppCompatActivity {
 
     private Button startB;
-    EditText editText1,editText2;
+    EditText editText1,editText2,editText3;
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
@@ -20,8 +20,7 @@ public class JoData extends AppCompatActivity {
 
         editText1=findViewById(R.id.fromeF);
         editText2=findViewById(R.id.ToF);
-
-
+        editText3=findViewById(R.id.editTextTextPersonName);
 
 
         startB = findViewById(R.id.start);
@@ -31,8 +30,13 @@ public class JoData extends AppCompatActivity {
             public void onClick(View view) {
                 String value1 = editText1.getText().toString().trim();
                 String value2 = editText2.getText().toString().trim();
-                startActivity(new Intent(JoData.this, journey.class));
+                String value3 = editText3.getText().toString().trim();
 
+                Intent intent=new Intent(JoData.this, journey.class);
+                intent.putExtra("value1name",value1);
+                intent.putExtra("value2name",value2);
+                intent.putExtra("value3name",value3);
+                startActivity(intent);
             }
         });
     }
--- Mobus1/app/src/main/java/com/example/mobus1/journey.java
+++ Mobus1/app/src/main/java/com/example/mobus1/journey.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,11 +12,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class journey extends AppCompatActivity {
     private Button startC;
+    TextView  textView,textView1,textView2,textView3;
+    int temp;
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.journey_start);
 
+        textView=findViewById(R.id.textView3);
+        textView1=findViewById(R.id.textView4);
+        textView2=findViewById(R.id.textView8);
+        textView3=findViewById(R.id.textView10);
+        String value1=getIntent().getStringExtra("value1name");
+        String value2=getIntent().getStringExtra("value2name");
+        String value3=getIntent().getStringExtra("value3name");
+        textView1.setText(value3);
+        if(value1.equals("Silicon Hills") && value2.equals("Damana square"))
+        {
+            textView.setText("4Km");
+            temp=Integer.parseInt(value3)*5;
+            textView2.setText(String.valueOf(temp));
+        }
+        else if(value1.equals("Silicon Hills") && value2.equals("Trident"))
+        {
+            textView.setText("2Km");
+            temp=Integer.parseInt(value3)*5;
+            textView2.setText(String.valueOf(temp));
+        }
+        else if(value1.equals("Silicon Hills") && value2.equals("Acharya vihar"))
+        {
+            textView.setText("8Km");
+            temp=Integer.parseInt(value3)*10;
+            textView2.setText(String.valueOf(temp));
+        }
+        textView3.setText(String.valueOf(100-temp));
         startC = findViewById(R.id.button2);
         startC.setOnClickListener(new View.OnClickListener() {
             @Override
